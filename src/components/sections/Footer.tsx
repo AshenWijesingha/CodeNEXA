50126818467fa7801388885b3162f58e8c618dd0
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-22 00:46:35 +0530

    Added footer section

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,11 +1,13 @@
-const Footer = () => (
-<footer className="bg-gray-800 py-6 mt-10">
-  <div className="container mx-auto text-center">
-    <p className="text-gray-400 text-sm md:text-base">
-      © 2024 All rights reserved. This webpage is developed by <strong>Project ALVI Team 👾</strong> with ❤️🫂
-    </p>
-  </div>
-</footer>
-);
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-6 mt-10">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-400 text-sm md:text-base">
+          © 2024 All rights reserved. This webpage is developed by <strong>Project ALVI Team 👾</strong> with ❤️🫂
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
