204b0537059a553fb3c5c4c72b082eeefafa5b44
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-22 00:21:11 +0530

    Add Mobile Responsiveness in to Time Line

--- src/components/sections/Timeline.tsx
+++ src/components/sections/Timeline.tsx
@@ -72,15 +72,15 @@ const Milestones = () => {
             <li key={index} className="mb-10 ml-6">
               <div className="absolute -left-3.5 mt-1 w-7 h-7 bg-blue-500 rounded-full border border-white"></div>
               <div className="p-6 bg-white shadow-lg rounded-lg transform transition hover:scale-105">
-                <p className="text-gray-600 text-l">{milestone.date}</p>
-                <h3 className="text-4xl font-semibold mb-2">{milestone.title}</h3>
-                <p className="text-gray-700 mb-4 text-2xl">{milestone.description}</p>
-                <p className="text-gray-700 font-bold text-xl">
+                <p className="text-gray-600 text-sm md:text-base">{milestone.date}</p>
+                <h3 className="text-2xl md:text-4xl font-semibold mb-2">{milestone.title}</h3>
+                <p className="text-gray-700 mb-4 text-lg md:text-2xl">{milestone.description}</p>
+                <p className="text-gray-700 font-bold text-lg md:text-xl">
                   Marks Allocated: {milestone.marks}
                 </p>
-                <div className="w-full bg-gray-200 rounded-full h-4 mt-3">
+                <div className="w-full bg-gray-200 rounded-full h-3 md:h-4 mt-3">
                   <div
-                    className="bg-blue-500 h-5 rounded-full text-xl text-white flex justify-center items-center"
+                    className="bg-blue-500 h-4 md:h-5 rounded-full text-xs md:text-xl text-white flex justify-center items-center"
                     style={{ width: `${milestone.marks}%` }}
                   >
                     {milestone.marks}%
