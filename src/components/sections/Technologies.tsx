49907ad8a665c9cf6fdd0a8532543b46a1950f59
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-21 22:35:54 +0530

    Updated Technologies

--- src/components/sections/Technologies.tsx
+++ src/components/sections/Technologies.tsx
@@ -13,7 +13,7 @@ const TechnologiesUsed = () => {
     { name: 'Kafka', image: `${baseDomain}Images/tech/kafka.jpg` },
     { name: 'Docker', image: `${baseDomain}Images/tech/doker.png` },
     { name: 'Google Colab', image: `${baseDomain}Images/tech/colab.png` },
-    { name: 'Google map API', image: `${baseDomain}Images/tech/googlemap.png` },
+    { name: 'Amazon AWS', image: `https://imgs.search.brave.com/DkXtxsh5abUFA13ArNlndiAftLFNG8EY5oVlGhD4NWs/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi85LzkzL0Ft/YXpvbl9XZWJfU2Vy/dmljZXNfTG9nby5z/dmcvMjIwcHgtQW1h/em9uX1dlYl9TZXJ2/aWNlc19Mb2dvLnN2/Zy5wbmc` },
     { name: 'JWT', image: `${baseDomain}Images/tech/jwt.png` }
   ];
 
