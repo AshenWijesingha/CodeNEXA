2ea79b09d22d101b3be982d5a3133828052731d3
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-21 23:58:28 +0530

    Add Mobile Responsivenes to the Scroling Text

--- src/components/ui-component/ScrollingText.tsx
+++ src/components/ui-component/ScrollingText.tsx
@@ -5,9 +5,9 @@ const ScrollingText = () => {
     <div>
         <br /><br />
     <VelocityScroll
-      text="CodeNexa by Team Alvi" 
+      text="CodeNexa by Team Alvi"
       default_velocity={5}
-      className="font-display text-center text-xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+      className="font-display text-center text-2xl sm:text-4xl md:text-7xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:leading-[5rem]"
     />
     <br /><br />
     </div>
