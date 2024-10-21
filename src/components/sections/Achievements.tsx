9fee98658079d102ee8af50ec307bfb6c110b83d
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-21 22:24:42 +0530

    Updated Achivements

--- src/components/sections/Achievements.tsx
+++ src/components/sections/Achievements.tsx
@@ -6,8 +6,7 @@ const AchievementsSection = () => {
         <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
        Achievements
        <br /><br />
-      </span>     
-      <br /><br />   
+      </span>  
         <div className="flex justify-center">
           <div className="text-center">
             <img
