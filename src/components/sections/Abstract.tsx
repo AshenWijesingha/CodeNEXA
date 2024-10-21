fa570be0db1de5ad592d0cafefed74fcf275d581
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-21 23:39:54 +0530

    Changed the Topic Font Size for Mobile Responsivenes

--- src/components/sections/Abstract.tsx
+++ src/components/sections/Abstract.tsx
@@ -6,9 +6,16 @@ const Abstract = () => (
       color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
     > */}
 
-      <span id="LiteratureSurvey" className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+      {/* <span id="LiteratureSurvey" className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Literature Survey
-      </span>
+      </span> */}
+
+      <span
+  id="LiteratureSurvey"
+  className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-4xl sm:text-6xl md:text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
+>
+  Literature Survey
+</span>
       
       <br /><br />
       <div className="max-w-4xl mx-auto">
