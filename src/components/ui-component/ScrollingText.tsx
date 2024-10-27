1b328abd0cf685fc3c5aef736ac61fb7062bdbb9
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-27 18:40:32 +0530

    Updated Scroing Texts

--- src/components/ui-component/ScrollingText.tsx
+++ src/components/ui-component/ScrollingText.tsx
@@ -5,7 +5,7 @@ const ScrollingText = () => {
     <div>
         <br /><br />
     <VelocityScroll
-      text="Project ALVI"
+      text="Project ALVI by Team FedGuardians"
       default_velocity={5}
       className="font-display text-center text-2xl sm:text-4xl md:text-7xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:leading-[5rem]"
     />
