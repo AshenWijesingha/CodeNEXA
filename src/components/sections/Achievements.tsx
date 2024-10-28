65e031c0cd38520ddc3ece7d18b3c1a552f32d57
AshenWijesingha <a.d.wijesingha@gmail.com>
2024-10-28 12:35:31 +0530

    Updating Achievements

--- src/components/sections/Achievements.tsx
+++ src/components/sections/Achievements.tsx
@@ -22,6 +22,38 @@ const AchievementsSection = () => {
             Our research was selected for the IEEE UEMCON Awards 2024 conference, and we have already presented our paper at the event.
           </p>
         </div>
+
+        <div className="flex justify-center">
+          <div className="text-center">
+            <img
+              src="/achievements/ICAC.png"
+              alt="Award"
+              className="w-4/5 mx-auto"
+            />
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <p className="text-2xl text-gray-700 leading-relaxed">
+            Our paper was selected for the ICAC 2024 conference, and we are now ready to present it at the conference.
+          </p>
+        </div>
+
+        <div className="flex justify-center">
+          <div className="text-center">
+            <img
+              src="/achievements/SNAMS.png"
+              alt="Award"
+              className="w-4/5 mx-auto"
+            />
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <p className="text-2xl text-gray-700 leading-relaxed">
+            Our paper was selected for the SNAMS 2024 conference, and we are now ready to present it at the event.
+          </p>
+        </div>
       </div>
     </section>
   );
